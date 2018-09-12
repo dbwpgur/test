5e4d1fe0e248ca71d6cfd125c35741c55953d0e4
Unknown <dbwpgur@naver.com>
2018-09-12 16:06:02 +0900

    스프링 프로젝트 생성및데이터전달예제

---
+++ src/main/java/kr/green/spring/controller/HomeController.java
@@ -0,0 +1,57 @@
+package kr.green.spring.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model, Integer num, Integer num2 ,Integer res) {
+		model.addAttribute("num", num);
+		model.addAttribute("num2", num2);
+		
+		model.addAttribute("res", res);
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String testGet(Model model) {
+		model.addAttribute("company", "그린");
+		return "test/test";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+
+	public String testPost(Model model, Integer num, Integer num2) {
+
+		System.out.println("숫자1: " + num);
+		System.out.println("숫자1: " + num2);
+		if (num == null)
+			num = 0;
+		if (num == null)
+			num = 0;
+		Integer res = num + num2;
+		model.addAttribute("num", num);
+		model.addAttribute("num2", num2);
+		model.addAttribute("res", res);
+		return "redirect:/";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/test/test.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>여기는 ${company}입니다.
+	<form action="/spring/test" method="post">
+		<input name="num">
+		<input name="num2">
+		<button type ="submit">제출</button>
+	</form>
+
+</body>
+</html>
